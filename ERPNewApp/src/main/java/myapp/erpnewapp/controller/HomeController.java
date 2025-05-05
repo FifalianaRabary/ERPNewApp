ce4ae4b9111c056f172bec5185a1a65f8dd6a485
FifalianaRabary <158579041+FifalianaRabary@users.noreply.github.com>
2025-05-05 10:45:22 +0300

    liste purchase order

--- ERPNewApp/src/main/java/myapp/erpnewapp/controller/HomeController.java
+++ ERPNewApp/src/main/java/myapp/erpnewapp/controller/HomeController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpSession;
+import myapp.erpnewapp.model.ErpNextSessionInfo;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
     public String home(HttpSession session, Model model) {
-        String username = (String) session.getAttribute("username");
-        if (username == null) return "redirect:/login";
+        ErpNextSessionInfo info = (ErpNextSessionInfo) session.getAttribute("info");
+        if (info == null) return "redirect:/login";
 
-        model.addAttribute("username", username);
+        model.addAttribute("username", info.getFullName());
         return "home";
     }
 }
